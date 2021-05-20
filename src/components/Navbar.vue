7a89814cb5626849fa1379ee6d23f1978d4ca738
michael beloff <michael.a.beloff@gmail.com>
2021-05-21 08:30:45 +1000

    hide data

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -18,7 +18,7 @@
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item
+            <!-- <b-nav-item
               class="text-right rounded"
               v-for="(region, i) in $store.state.global"
               :key="i"
@@ -31,7 +31,7 @@
                 }
               }"
               >{{ region.name }}
-            </b-nav-item>
+            </b-nav-item> -->
           </b-navbar-nav>
           <b-navbar-nav class="ml-auto">
             <b-nav-item
